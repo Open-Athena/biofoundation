40f02798ad1c8d8a5eba9740eb4d09d8b6a85c17
Gonzalo Benegas <gsbenegas@gmail.com>
2026-01-14 21:43:01 +0000

    Add HF embedding adapter and example

--- biofoundation/model/adapters/hf.py
+++ biofoundation/model/adapters/hf.py
@@ -3,7 +3,7 @@ from torch import Tensor
 from typing import cast
 from transformers import PreTrainedModel, PreTrainedTokenizerBase
 
-from biofoundation.model.base import CausalLM, MaskedLM, Tokenizer
+from biofoundation.model.base import CausalLM, EmbeddingModel, MaskedLM, Tokenizer
 
 
 class HFMaskedLM(MaskedLM):
@@ -24,6 +24,26 @@ class HFCausalLM(CausalLM):
 
     def forward(self, input_ids: Int[Tensor, "B L"]) -> Float[Tensor, "B L V"]:
         return cast(Float[Tensor, "B L V"], self._model(input_ids).logits)
+
+
+class HFEmbeddingModel(EmbeddingModel):
+    def __init__(self, model: PreTrainedModel, layer: str = "last"):
+        assert model.__class__.__name__.endswith("Model")
+        super().__init__()
+        self._model = model
+        self._layer = layer
+        assert layer in ["last", "middle"]
+
+    def forward(self, input_ids: Int[Tensor, "B L"]) -> Float[Tensor, "B L D"]:
+        if self._layer == "last":
+            output = self._model(input_ids=input_ids)
+            hidden_state = output.last_hidden_state
+        elif self._layer == "middle":
+            output = self._model(input_ids=input_ids, output_hidden_states=True)
+            hidden_states = output.hidden_states  # Tuple of all layer outputs
+            middle_idx = len(hidden_states) // 2
+            hidden_state = hidden_states[middle_idx]
+        return cast(Float[Tensor, "B L D"], hidden_state)
 
 
 class HFTokenizer(Tokenizer):
